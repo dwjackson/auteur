4de232adb144dc046692fc83b62daf7fa2976d75
David Jackson <dwjackson@users.noreply.github.com>
2021-04-16 08:29:39 -0400

    Start collecting script features before printing
    Rather than print directly as we read the file, we collect up a list of
    script features which will be printed after we've read the whole file.

--- parser.c
+++ parser.c
@@ -3,8 +3,12 @@
 #include "auteur_postscript.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define TOP_OF_PAGE (PAGE_HEIGHT - MARGIN_TOP)
+
+#define FEATURE_BUFSIZE_MIN 20
 
 void parser_init(struct parser *parser)
 {
@@ -12,6 +16,8 @@ void parser_init(struct parser *parser)
 	parser_reset_vpos(parser);
 	parser->feat = F_NONE;
 	parser->print_func = PRINT_LEFT;
+	parser->features = NULL;
+	parser->last_feature = NULL;
 }
 
 void parser_reset_vpos(struct parser *parser)
@@ -39,5 +45,63 @@ void parser_newline(struct parser *parser)
 			printf("(%d.) print_page_number\n", parser->pos.page_num);
 		}
 		printf("align_start\n");
+	}
+}
+
+void parser_feature_start(struct parser *parser, enum script_feature_type feature_type)
+{
+	struct script_feature *feature = malloc(sizeof(struct script_feature));
+	if (feature == NULL) {
+		abort();
+	}
+
+	feature->sf_type = feature_type;
+	feature->sf_size = FEATURE_BUFSIZE_MIN;
+	feature->sf_len = 0;
+	feature->next = NULL;
+	feature->sf_buf = malloc(feature->sf_size);
+	memset(feature->sf_buf, 0, feature->sf_size);
+	if (feature->sf_buf == NULL) {
+		abort();
+	}
+
+	if (parser->features == NULL) {
+		parser->features = feature;
+	}
+	parser->last_feature = feature;
+}
+
+void parser_feature_append_text(struct parser *parser, const char *text, size_t len)
+{
+	struct script_feature *feat = parser->last_feature;
+	while (feat->sf_len + len + 1 > feat->sf_size) {
+		feat->sf_size *= 2;
+		feat->sf_buf = realloc(feat->sf_buf, feat->sf_size);
+		if (feat->sf_buf == NULL) {
+			abort();
+		}
+	}
+	strncat(feat->sf_buf, text, len);
+	feat->sf_len += len;
+	feat->sf_buf[feat->sf_len] = '\0';
+}
+
+void parser_print_features(struct parser *parser)
+{
+	struct script_feature *curr;
+	for (curr = parser->features; curr != NULL; curr = curr->next) {
+		/* TODO */
+	}
+}
+
+void parser_free_features(struct parser *parser)
+{
+	struct script_feature *prev = NULL;
+	struct script_feature *curr = parser->features;
+	while (curr != NULL) {
+		prev = curr;
+		curr = curr->next;
+		free(prev->sf_buf);
+		free(prev);
 	}
 }
--- parser.h
+++ parser.h
@@ -1,6 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdlib.h>
+
 /* Directives */
 #define DIRECTIVE_LEN 3
 #define D_TITLE ".Tl"
@@ -26,6 +28,14 @@ enum script_feature_type {
 	F_DIALOGUE
 };
 
+struct script_feature {
+	enum script_feature_type sf_type;
+	char *sf_buf;
+	size_t sf_size;
+	size_t sf_len;
+	struct script_feature *next;
+};
+
 struct position {
 	double hpos;
 	double vpos;
@@ -36,11 +46,17 @@ struct parser {
 	struct position pos;
 	enum script_feature_type feat;
 	const char *print_func;
+	struct script_feature *features;
+	struct script_feature *last_feature;
 };
 
 void parser_init(struct parser *parser);
 void parser_reset_vpos(struct parser *parser);
 void parser_reset_hpos(struct parser *parser);
 void parser_newline(struct parser *parser);
+void parser_feature_start(struct parser *parser, enum script_feature_type feature_type);
+void parser_feature_append_text(struct parser *parser, const char *text, size_t len);
+void parser_print_features(struct parser *parser);
+void parser_free_features(struct parser *parser);
 
 #endif /* PARSER_H */
